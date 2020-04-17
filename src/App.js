988e29b616448edbde6b0ef68a245a7b20564fb8
Mike Blom <mikeblom@Mikes-MacBook-Pro.local>
2020-04-17 10:47:03 +0100

    Adding Authentication Login With Hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Home from './Home';
+import Login from './Login';
 import Speakers from './Speakers';
 
 export const ConfigContext = React.createContext();
 
 const configValue = {
   showSignMeUp: true,
-  showSpeakerSpeakingDays: true,
-  loggedInUserEmail: 'mike@test.co'
+  showSpeakerSpeakingDays: true
 };
 
 const pageToShow = pageName => {
   if (pageName === 'Home') return <Home />;
   if (pageName === 'Speakers') return <Speakers />;
+  if (pageName === 'Login') return <Login />;
+
   return <div>Not Found</div>;
 };
 
-const App = ({ pageName }) => {
+const App = ({ pageName, userInfo }) => {
+  configValue.loggedInUserEmail = userInfo ? userInfo.email : '';
   return (
     <ConfigContext.Provider value={configValue}>
       <div>{pageToShow(pageName)}</div>;
